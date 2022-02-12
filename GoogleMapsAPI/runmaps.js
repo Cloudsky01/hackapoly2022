413af2bed74eeae583dd13d7dacd0129674ec8df
Matcorb <mathieu.corbishley@polymtl.ca>
2022-02-12 16:46:15 -0500

    changements google maps api

--- GoogleMapsAPI/runmaps.js
+++ GoogleMapsAPI/runmaps.js
@@ -1,14 +1,51 @@
 function initMap() {
     // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
+    const poly = { lat: 45.50485816337779, lng: -73.61298294899296 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
+      zoom: 12,
+      center: poly,
+      mapId: "c16185d6f3a5a96e"
     });
     // The marker, positioned at Uluru
     const marker = new google.maps.Marker({
-      position: uluru,
+      position: poly,
       map: map,
     });
   }
+
+// var origin1 = new google.maps.LatLng(55.930385, -3.118425);
+// var origin2 = 'Greenwich, England';
+// var destinationA = 'Stockholm, Sweden';
+// var destinationB = new google.maps.LatLng(50.087692, 14.421150);
+
+// var service = new google.maps.DistanceMatrixService();
+// service.getDistanceMatrix(
+//   {
+//     origins: [origin1, origin2],
+//     destinations: [destinationA, destinationB],
+//     travelMode: 'DRIVING',
+//     transitOptions: TransitOptions,
+//     drivingOptions: DrivingOptions,
+//     unitSystem: UnitSystem,
+//     avoidHighways: Boolean,
+//     avoidTolls: Boolean,
+//   }, callback);
+
+// function callback(response, status) {
+//     if (status == 'OK') {
+//         var origins = response.originAddresses;
+//         var destinations = response.destinationAddresses;
+
+//         for (var i = 0; i < origins.length; i++) {
+//             var results = response.rows[i].elements;
+//             for (var j = 0; j < results.length; j++) {
+//                 var element = results[j];
+//                 var distance = element.distance.text;
+//                 var duration = element.duration.text;
+//                 var from = origins[i];
+//                 var to = destinations[j];
+//             }
+//         }
+//     }
+// }
